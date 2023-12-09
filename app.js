b54112407085b97528150db42ebbd8fe6c3c4bdc
deepak <deepamdeebu@gmail.com>
2023-12-09 15:48:49 +0530

    Crud : Provide feature to create and delete the list of items and some beautify

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.set('view engine', 'ejs');
 // middleware & static files
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: true }));
+
 // Sample values that can be got for each request in middleware
 app.use((req, res, next) => {
     console.log('new request made:');
@@ -35,6 +37,11 @@ app.use((req, res, next) => {
 
 // logging
 app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
 
 // mongoose & mongo tests
 app.get('/add-blog', (req, res) => {
@@ -53,7 +60,6 @@ app.get('/add-blog', (req, res) => {
       });
 });
 
-
 app.get('/', (req, res) => {
   // res.send('<p>home page</p>');
   const blogs = [
@@ -70,6 +76,53 @@ app.get('/about', (req, res) => {
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create a new blog' });
+});
+
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+      .then(result => {
+        res.render('index', { blogs: result, title: 'All blogs' });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+});
+
+app.post('/blogs', (req, res) => {
+    console.log("backend input \n");
+    console.log(req.body);
+    const blog = new Blog(req.body);
+
+    blog.save()
+      .then(result => {
+        res.redirect('/blogs');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+});
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+      .then(result => {
+        res.render('details', { blog: result, title: 'Blog Details' });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+});
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+      .then(result => {
+        res.json({ redirect: '/blogs' });
+      })
+      .catch(err => {
+        console.log(err);
+      });
 });
 
 // redirects
